7cbbf390d0781ae544d557f1ccdf307fe9aa6b87
Lokesh7729 <lokeshchauhan7729@gmail.com>
2024-05-02 00:31:31 +0530

    first commit

---
+++ first.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+//  #include is preprocessor directive -> before execting program we need to include some file i.e iostream file  
+// iostream is a library file in which i means input and o means output 
+
+using namespace std;
+//  namespace is area or module or set of files
+// we want to use featues form std 
+int main(){
+    // staring point
+    // int is return type and main() is function name
+    // curly braces define scope 
+
+
+    // cout<<"lokesh chauhan"<<endl; // cout is used to print output
+    // cout + << = print kardo data ko
+    // endl means to go next line 
+    // ; means terminaiton of program 
+
+
+
+    // input and output
+    // for storing the age of a person age we need a container i.e variable -> so we will declare varibel 
+
+    // int agee ; // int is a datatype and age is a variable 
+    // cout<<"Enter your age Sanjana : ";
+    // cin>>agee;
+    // cout <<"Your age is "<<agee<<endl;
+
+
+
+
+    // varible decalaration
+    // int age;
+    // variable initialisation
+    // int age=20;
+    // assign the value of age 
+
+    // char ch='a'; // single '' are used 
+    // char ch2='abc';
+    // cout<<ch2<<endl;
+
+    // float f =11.45;
+    // double d = 12.134252525;
+
+
+    char ch='a';
+    cout<<sizeof(ch);
+
+
+
+
+
+
+    return 0;// it tells successfull execution
+}
