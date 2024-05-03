a4730cb35a5f83de95cd8334f39353ea143f5dea
Lokesh7729 <lokeshchauhan7729@gmail.com>
2024-05-04 01:53:58 +0530

    print table of a number

---
+++ Daily Coding Problem/Day 3/printTable.cpp
@@ -0,0 +1,19 @@
+// Problem 3:
+// Print table of a number
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int number;
+    cout<<"Enter a number : ";
+    cin>>number;
+
+    for (int i = 1; i <=10; i++)
+    {
+        cout<<number<<" x "<<i<<" = "<<number*i<<endl;
+    }
+    
+    return 0;
+}
