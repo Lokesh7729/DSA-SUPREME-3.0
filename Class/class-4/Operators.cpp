2efe2a0aa60cbad48f7c1b87372409770ea5f18f
Lokesh7729 <lokeshchauhan7729@gmail.com>
2024-05-07 20:51:58 +0530

    Operators, conditionals, loops

---
+++ Class/class-4/Operators.cpp
@@ -0,0 +1,63 @@
+// Operators -> are symbol that carry special meaning
+// Operand -> the value in which we perform the operation
+
+// 1-> Arithmetic -> + , - , * , / , % 
+// 2-> Relational -> > , < , >= , <= , == , != -> output (true or false)
+// 3-> Assignment -> = , += , -=, *=, /= , %=
+// 4-> Logical    -> && , || , ! 
+// 5-> Bitwise
+
+
+//  unary-> single operand -> 
+    // 1-> ++ (Increment)
+    //  a++ ->post-increment d
+    // ++a -> pre increment
+
+    // 2-> -- (Decrement)
+    // a-- -> post-decrement
+    // --a -> pre-decrement
+
+
+
+
+//  Binary-> two operand
+
+
+// boolean -> true 1 
+//         -> false 0
+
+// initialization -> int a=5;
+
+
+// CONDITIONALS 
+
+ /*
+ if statement
+ if- else 
+ if -else if 
+ if else if else
+ nestd if  
+ 
+ */
+
+
+// ternary operator
+// (condition)? statement 1 : statement 2;
+
+
+
+// Switch case
+
+// switch (expression)
+// {
+// case /* constant-expression */:
+//     /* code */
+//     break;
+
+// default:
+//     break;
+// }
+
+
+
+// loops -> set of instructions multiple time
