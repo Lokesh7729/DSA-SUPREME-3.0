3b5fcbde0d136e4719528e7c8d5097b2729de16c
Lokesh7729 <lokeshchauhan7729@gmail.com>
2024-06-15 21:17:46 +0530

    numeric palindrome equilateral pyramid

--- Patterns/tempCodeRunnerFile.cpp
+++ Patterns/tempCodeRunnerFile.cpp
@@ -1,26 +1,3 @@
-void invertedPyramid(int n){
+se{
 
-    for (int  row = 0; row < n; row++)
-    {   
-        // 1.> spaces
-        for (int col = 0; col < row; col++)
-        {
-            cout<<" ";
-        }
-        // 2.> stars
-         for (int col = 0; col < n-row; col++)
-         {
-            if (row==0 || row==n-1 || col==0|| col==n-row-1)
-            {
-                cout<<"* ";
-            }else{
-                cout<<"  ";
-            }
-            
-         }
-            cout<<endl;
-         
-        
-    }
-    
-}
+            // }
