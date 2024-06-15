6c570c812fc3a8b43d79c71be9144536415f194d
Lokesh7729 <lokeshchauhan7729@gmail.com>
2024-06-16 04:57:05 +0530

    solid half diamond

---
+++ Patterns/24_solid_half_Diamond.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    for (int i = 0; i < 2 * n - 1; i++)
+    {
+        int cond = 0;
+        if (i < n)  // growing phase of diamond
+        {
+            cond = i;
+        }
+        else    // shrinking phase of diamond
+        {
+            cond= n - (i%n)-2;
+            
+        }
+         
+        
+
+        for (int j = 0; j <= cond; j++)
+        {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+}
