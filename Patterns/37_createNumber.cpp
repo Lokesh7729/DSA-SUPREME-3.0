4c90400fc9569fac2f037aca7422cf9a13442133
Lokesh7729 <lokeshchauhan7729@gmail.com>
2024-06-18 00:05:08 +0530

    create number using digits

---
+++ Patterns/37_createNumber.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int createNumberUsingDigits(int numberOfDigits){
+    int num=0;
+    for (int i = 0; i < numberOfDigits; i++)
+    {
+        cout<<"enter digit: "<<endl;
+        int digit;
+        cin>>digit;
+        num=num*10+digit;
+        cout<<"number created so far : "<<num<<endl;
+    }
+    return num;
+}
+
+int main(){
+    int numberOfDigits;
+    cin>>numberOfDigits;
+
+    int num= createNumberUsingDigits(numberOfDigits);
+    cout<<"number created by digits : "<<num;
+}
