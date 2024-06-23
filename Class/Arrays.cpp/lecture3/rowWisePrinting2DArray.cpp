cc8bca50b5c80a43044da76b353beeb121071b54
Lokesh7729 <lokeshchauhan7729@gmail.com>
2024-06-24 00:10:51 +0530

    row wise sum in 2d array

---
+++ Class/Arrays.cpp/lecture3/rowWisePrinting2DArray.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+
+int findMinmunIn2DArray(int arr[][4], int rowSize, int colSize ){
+    int minValue= INT_MAX;
+    for (int i = 0; i < rowSize; i++)
+    {
+       for (int j = 0; j < colSize; j++)
+       {
+        minValue= min(arr[i][j], minValue);
+
+       }
+    }
+    return minValue;
+}
+
+int rowWiseSum(int arr[][3], int rowSize, int colSize ){
+    for (int i = 0; i < rowSize; i++)
+    {
+        int sum =0;
+       for (int j = 0; j < colSize; j++)
+       {
+        sum = sum+ arr[i][j];
+    }
+    cout<<sum<<" ";
+}
+}
+int main(){
+    int arr[3][3]={
+        {10,20,30},
+        {40,50,60},
+        {70,80,90}
+    };
+
+    int rowSize=3;
+    int colSize=3;
+
+int min = rowWiseSum(arr, rowSize,colSize);
+
+
+
+}
+
