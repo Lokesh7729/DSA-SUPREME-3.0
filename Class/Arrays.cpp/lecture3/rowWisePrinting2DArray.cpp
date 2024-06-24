348542fe83b11c0f887531a56980dbff6dc8ec65
Lokesh7729 <lokeshchauhan7729@gmail.com>
2024-06-24 07:37:25 +0530

    diagonal sum

--- Class/Arrays.cpp/lecture3/rowWisePrinting2DArray.cpp
+++ Class/Arrays.cpp/lecture3/rowWisePrinting2DArray.cpp
@@ -41,18 +41,53 @@ int colWiseSum(int arr[][3], int rowSize, int colSize ){
 cout<<endl;
 }
 
+void diagonalWiseSum(int arr[][3], int rowSize, int colSize ){
+    int sum =0;
+    for (int i = 0; i < rowSize; i++)
+    {
+      for (int j = 0; j < colSize; j++)
+      {
+        if (i+j==2)
+        {
+            sum = sum + arr[i][j];
+        }
+        
+      }
+      
+        
+    }
+    
+
+    cout<<sum;
+
+
+//     int sum =0;
+//     for (int i = 0; i < rowSize; i++)
+//     {
+//        for (int j = 0; j < colSize; j++)
+//        {
+//         if (i==j)
+//         {
+//           sum = sum+ arr[j][j];
+//         }
+        
+//     }
+// }
+// cout<<sum<<endl;
+}
 int main(){
     int arr[3][3]={
         {10,20,30},
         {40,50,60},
-        {70,80,90}
+        {50,80,90}
     };
 
     int rowSize=3;
     int colSize=3;
 
-int rowSum = rowWiseSum(arr, rowSize,colSize);
-int colSum = colWiseSum(arr, rowSize, colSize);
+// int rowSum = rowWiseSum(arr, rowSize,colSize);
+// int colSum = colWiseSum(arr, rowSize, colSize);
+diagonalWiseSum(arr, rowSize, colSize);
 
 
 
