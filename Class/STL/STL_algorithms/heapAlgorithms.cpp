3826e4452b850e5fd31f1e9874f56209d7345d92
Lokesh7729 <lokeshchauhan7729@gmail.com>
2024-07-04 09:25:17 +0530

    heapAlgorithms

---
+++ Class/STL/STL_algorithms/heapAlgorithms.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+
+    vector<int> arr;
+    arr.push_back(10);
+    arr.push_back(12);
+    arr.push_back(13);
+    arr.push_back(15);
+    arr.push_back(20);
+    // max heap
+    make_heap(arr.begin(), arr.end());
+
+    // for(int a: arr){
+    //     cout<<a<<" ";
+    // }
+    // cout<<endl;
+    arr.push_back(99);
+    push_heap(arr.begin(), arr.end());
+
+    for (int a : arr)
+    {
+        cout << a << " ";
+    }
+    cout << endl;
+
+    // deletion
+    pop_heap(arr.begin(), arr.end());
+    arr.pop_back();
+    for (int a : arr)
+    {
+        cout << a << " ";
+    }
+    cout << endl;
+
+
+    // sort heap
+    sort_heap(arr.begin(),arr.end());
+    
+
+    return 0;
+}
