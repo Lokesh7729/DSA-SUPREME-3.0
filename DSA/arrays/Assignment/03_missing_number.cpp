6a5239cca648d3364a77578117c7b8a6e16f5a69
Lokesh7729 <lokeshchauhan7729@gmail.com>
2024-10-02 04:03:52 +0530

    missing numbers - fast

--- DSA/arrays/Assignment/03_missing_number.cpp
+++ DSA/arrays/Assignment/03_missing_number.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 
+// 
 int sortNumber(vector<int>&nums){
     sort(nums.begin(),nums.end());
 
@@ -9,6 +10,19 @@ int sortNumber(vector<int>&nums){
         else return i;
     }  
 return nums.size();
+}
+
+int xorr(vector<int>nums){
+    int ans= 0 ;
+    // xor all values of array
+    for(int i=0; i<nums.size();i++){
+        ans ^= nums[i];
+    }
+    // xor all range items [0,n]
+    for(int i=0; i <= nums.size();i++){
+        ans^= i;
+    }
+    return ans;
 }
 
 int missingNumber(vector<int>& nums) {
