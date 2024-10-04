12228d4f984b1e6c8c8e7d91a27973e6512db131
Lokesh7729 <lokeshchauhan7729@gmail.com>
2024-10-04 22:04:45 +0530

    missing element from an array with duplicates using shorting + swapping  method

--- DSA/arrays/Assignment/09_missing_element_from_an_array_with_duplicates.cpp
+++ DSA/arrays/Assignment/09_missing_element_from_an_array_with_duplicates.cpp
@@ -1,39 +1,68 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void findMissing(int*a,int n){
+void findMissing(int *a, int n)
+{
 
-    // visited method 
-    for (int i = 0; i < n; i++)
-    {
-        int index= abs(a[i]);
-        if (a[index-1]>0)
-        {
-            a[index-1] *= -1;
-        }
-    }
+    // visited method
+    // for (int i = 0; i < n; i++)
+    // {
+    //     int index= abs(a[i]);
+    //     if (a[index-1]>0)
+    //     {
+    //         a[index-1] *= -1;
+    //     }
+    // }
 
     // for (int i = 0; i < n; i++)
     // {
     //     cout<<a[i]<<" ";
     // }
 
-    // all positive indexes are missing 
-    
+    // all positive indexes are missing
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     if (a[i]>0)
+    //     {
+    //         cout<<i+1<<" ";
+    //     }
+    // }
+
+    // ! shorting + swapping method
+    int i = 0;
+    while (i < n)
+    {
+        int index = a[i] - 1;
+        if (a[i] != a[index])
+        {
+            swap(a[i], a[index]);
+        }
+        else
+        {
+            ++i;
+        }
+    }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout << a[i] << " ";
+    // }
+
     for (int i = 0; i < n; i++)
     {
-        if (a[i]>0)
+        if (a[i] != i + 1)
         {
-            cout<<i+1<<" ";
-        } 
+            cout << i + 1 << " ";
+        }
     }
 }
 
-int main(){
+int main()
+{
     int n;
-    int a[]={1,3,5,3,4};
-    n=sizeof(a)/sizeof(int);
+    int a[] = {1, 3, 5, 3, 4};
+    n = sizeof(a) / sizeof(int);
 
-    findMissing(a,n);
+    findMissing(a, n);
     return 0;
 }
