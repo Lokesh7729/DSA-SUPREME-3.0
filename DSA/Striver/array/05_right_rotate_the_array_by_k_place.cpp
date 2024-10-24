cbd87bcd9504d0cf796ee1f751b648c1a1e51841
Lokesh <lokeshchauhan7729@gmail.com>
2024-10-24 14:26:32 +0530

    right rotate the array by k place

---
+++ DSA/Striver/array/05_right_rotate_the_array_by_k_place.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+    int n= nums.size(); 
+    k= k%n;
+    if(k==0){
+        return;
+    }
+    
+    // storing to temp variable 
+    int temp[k];
+    for(int i=n-k;i<n;i++){
+        temp[i-(n-k)]=nums[i];
+    }
+    // shifting 
+    for(int i=n-k-1;i>=0;i--){
+        nums[i+k]=nums[i];
+    }
+    // putting back temp 
+    for(int i = 0;i<k;i++){
+        nums[i]=temp[i];
+    }
+
+    }
+};
